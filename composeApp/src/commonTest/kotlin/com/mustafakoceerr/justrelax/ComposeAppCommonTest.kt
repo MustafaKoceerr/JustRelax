721466e156dcec6d2d25fa43b0188719adc310db
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-11-15 15:20:39 +0300

    initial commit with love <3

---
+++ composeApp/src/commonTest/kotlin/com/mustafakoceerr/justrelax/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.mustafakoceerr.justrelax
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
