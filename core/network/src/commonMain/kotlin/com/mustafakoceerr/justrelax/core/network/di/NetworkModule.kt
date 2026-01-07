dc22bb308326c0690fa915a401298aa1c69f8353
MustafaKoceerr <mustafakoceerr@hotmail.com>
2026-01-07 23:15:52 +0300

    •	docs(core-network): remove redundant comments and polish documentation

--- core/network/src/commonMain/kotlin/com/mustafakoceerr/justrelax/core/network/di/NetworkModule.kt
+++ core/network/src/commonMain/kotlin/com/mustafakoceerr/justrelax/core/network/di/NetworkModule.kt
@@ -3,26 +3,17 @@ package com.mustafakoceerr.justrelax.core.network.di
 import com.mustafakoceerr.justrelax.core.domain.repository.legal.LegalRepository
 import com.mustafakoceerr.justrelax.core.domain.source.SoundRemoteDataSource
 import com.mustafakoceerr.justrelax.core.network.KtorClient
-import com.mustafakoceerr.justrelax.core.network.source.SoundRemoteDataSourceImpl
 import com.mustafakoceerr.justrelax.core.network.mapper.NetworkSoundToDomainMapper
 import com.mustafakoceerr.justrelax.core.network.source.LegalRepositoryImpl
+import com.mustafakoceerr.justrelax.core.network.source.SoundRemoteDataSourceImpl
 import io.ktor.client.HttpClient
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-/**
- * Bu Koin modülü 'public' olmalıdır ki ana uygulama (app modülü)
- * bu modülü Koin'e ekleyebilsin.
- */
 val networkModule = module {
-    // SoundMapper sınıfını 'single' olarak tanımlıyoruz.
-    // Uygulama boyunca tek bir tane SoundMapper nesnesi yeterli.
     singleOf(::NetworkSoundToDomainMapper)
 
-    // SoundRemoteDataSourceImpl sınıfını oluştur ve bunu
-    // SoundRemoteDataSource arayüzü olarak kaydet.
-    // Dışarıdan isteyenler arayüzü isteyecek, Koin onlara implementasyonu verecek.
     singleOf(::SoundRemoteDataSourceImpl) {
         bind<SoundRemoteDataSource>()
     }
@@ -33,4 +24,3 @@ val networkModule = module {
 
     single<HttpClient> { KtorClient }
 }
-
--- core/network/src/iosMain/kotlin/com/mustafakoceerr/justrelax/core/network/HttpClientEngineFactory.ios.kt
+++ core/network/src/iosMain/kotlin/com/mustafakoceerr/justrelax/core/network/HttpClientEngineFactory.ios.kt
@@ -5,9 +5,7 @@ import io.ktor.client.engine.darwin.Darwin
 
 internal actual fun createHttpClientEngine(): HttpClientEngine {
     return Darwin.create {
-        // iOS'e (Darwin) özel ayarlar burada yapılır
         configureRequest {
-            // Örneğin, hücresel veride de çalışmasına izin ver
             setAllowsCellularAccess(true)
         }
     }
