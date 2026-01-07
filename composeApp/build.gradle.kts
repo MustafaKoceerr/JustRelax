96fd5e34bdd301eab69cc94ae454091994df7b50
MustafaKoceerr <mustafakoceerr@hotmail.com>
2026-01-07 19:25:54 +0300

    •	chore(release): bump version to 1.1.2 (4)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mustafakoceerr.justrelax"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
     }
 
     buildTypes {
